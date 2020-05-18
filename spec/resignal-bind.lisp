0dd268a11ff85df3d58d8049a96a876a9018b4b7
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-05-18 22:50:03 +0900

    Cleanup: Spec format.

--- spec/resignal-bind.lisp
+++ spec/resignal-bind.lisp
@@ -25,21 +25,16 @@
 
 ;;; Condition handler
 ; When handler is found call it.
-#?(handler-bind((warning(lambda(c)
-			  (print(type-of c)))))
-    (signal 'warning))
+#?(handler-bind ((warning (lambda (c) (print (type-of c)))))
+  (signal 'warning))
 :outputs "
 WARNING "
 , :ignore-signals warning
 
 ; When control flow is not changed, nested some handlers are called.
-#?(handler-bind((warning(lambda(c)
-			  (declare(ignore c))
-			  (princ :outer))))
-    (handler-bind((warning(lambda(c)
-			    (declare(ignore c))
-			    (princ :inner))))
-      (signal 'warning)))
+#?(handler-bind ((warning (lambda (c) (declare (ignore c)) (princ :outer))))
+  (handler-bind ((warning (lambda (c) (declare (ignore c)) (princ :inner))))
+    (signal 'warning)))
 :outputs "INNEROUTER"
 , :ignore-signals warning
 
@@ -52,9 +47,9 @@ WARNING "
 :invokes-debugger NOT
 , :ignore-signals warning
 
-#?(with-output-to-string(*error-output*)
+#?(with-output-to-string (*error-output*)
      (warn "foo"))
-:satisfies (lambda($result)
+:satisfies (lambda ($result)
 	     (& (stringp $result)
 		(< 0 (length $result))))
 ,:ignore-signals warning
@@ -77,7 +72,7 @@ WARNING "
 ;;; CL:HANDLER-BIND
 ;; NOTE!
 ; CLHS say "type -- a type specifier", not limited only condition.
-#?(handler-bind((integer #'print))
+#?(handler-bind ((integer #'print))
     (concatenate 'string
 		 "above"
 		 "integer"
@@ -91,74 +86,77 @@ WARNING "
 ;;;; Description:
 ; capturing condition.
 ; Then resignaling another condition.
-#?(resignal-bind((error()'program-error))
+#?(resignal-bind ((error nil 'program-error))
     (error 'error))
 :signals program-error
 
 ;; NOTE!
 ; Just SIGNALING condition become invoking debugger.
-#?(resignal-bind((error()'program-error))
+#?(resignal-bind ((error nil 'program-error))
     (signal 'error))
 :invokes-debugger program-error
 
-#?(with-output-to-string(*error-output*)
-    (resignal-bind((warning()'warning))
+#?(with-output-to-string (*error-output*)
+    (resignal-bind ((warning nil 'warning))
       (signal 'warning)))
-:satisfies (lambda($result)
+:satisfies (lambda ($result)
 	     (& (stringp $result)
 		(< 0 (length $result))))
 , :ignore-signals warning
 
 ; Purpose is making better error message.
-#?(handler-case(resignal-bind((error()'simple-error
-				    :format-control "foo"))
-		 (error "bar"))
-    (error(c)(princ c)))
+#?(handler-case
+      (resignal-bind ((error nil 'simple-error :format-control "foo"))
+        (error "bar"))
+    (error (c)
+      (princ c)))
 :outputs "foo"
 
 ; it is invalid that downgrade error to warning,
 ; but upgrade warning to error.
-#?(resignal-bind((warning()'program-error))
-    (warn 'warning))
+#?(resignal-bind ((warning nil 'program-error)) (warn 'warning))
 :invokes-debugger program-error
 
-#?(resignal-bind((error()'warning))
-    (error 'error))
+#?(resignal-bind ((error nil 'warning)) (error 'error))
 :invokes-debugger warning
 , :ignore-signals warning
 
 ; same with CL:HANDLER-BIND, without signaling, nothing to do.
-#?(resignal-bind((error()'simple-error :format-control "why?"))
+#?(resignal-bind ((error nil 'simple-error :format-control "why?"))
     (+ 1 2))
 => 3
 
 ; same with CL:HANDLER-BIND, no binds is valid form.
-#?(resignal-bind()(+ 1 2))
+#?(resignal-bind () (+ 1 2))
 => 3
 
 ; same with CL:HANDLER-BIND, compound type specifier is valid.
-#?(resignal-bind(((not arithmetic-error)()'program-error))
-     (/ 1 (parse-integer "0")))
+#?(resignal-bind (((not arithmetic-error) () 'program-error))
+    (/ 1 (parse-integer "0")))
 :signals arithmetic-error
 , :lazy t
 
 ; like CL:HANDLER-CASE's clause,
 ; we can access captured condition when var is specified.
-#?(handler-case(resignal-bind((error(c)'simple-error
-				   :format-control(concatenate 'string
-							       (simple-condition-format-control c)
-							       " added string")))
-		 (error "error"))
-    (error(c)(princ c)))
+#?(handler-case
+    (resignal-bind
+        ((error (c) 'simple-error
+            :format-control (concatenate 'string
+                                         (simple-condition-format-control c)
+                                         " added string")))
+      (error "error"))
+    (error (c)
+      (princ c)))
 :outputs "error added string"
 
 ; Resignal-bind support status inheritance.
 ; When new condition has same slot with old condition,
 ; such value is inherited unless specified explicitly.
-#?(handler-case(resignal-bind((error()'simple-error
-				:format-arguments'(1 2)))
-		 (error "~S-~S":a :b))
-    (error(c)(princ c)))
+#?(handler-case
+      (resignal-bind ((error nil 'simple-error :format-arguments '(1 2)))
+        (error "~S-~S" :a :b))
+    (error (c)
+      (princ c)))
 :outputs "1-2" ; in this case, format-control is inherited.
                ; But format-arguments is superseded.
 
@@ -216,9 +214,9 @@ WARNING "
 #?(pprint-resignal-bind nil '(resignal-bind ((a nil))))
 :outputs "(RESIGNAL-BIND ((A ())))"
 
-#?(pprint-resignal-bind nil '(RESIGNAL-BIND((ERROR()'SIMPLE-ERROR
-                                              :FORMAT-CONTROL "Missing initform.~%~S"
-                                              :FORMAT-ARGUMENTS(LIST SLOT hoge)))
+#?(pprint-resignal-bind nil '(RESIGNAL-BIND ((ERROR () 'SIMPLE-ERROR
+                                               :FORMAT-CONTROL "Missing initform.~%~S"
+                                               :FORMAT-ARGUMENTS(LIST SLOT hoge)))
                                form))
 :outputs
 "(RESIGNAL-BIND ((ERROR () 'SIMPLE-ERROR
--- spec/resignal-bind.test.asd
+++ spec/resignal-bind.test.asd
@@ -1,7 +1,7 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem :resignal-bind.test
- :version "0.1.1"
+ :version "0.1.2"
  :depends-on (:jingoh "resignal-bind")
  :components ((:file "resignal-bind")) :perform
  (test-op (o c)
